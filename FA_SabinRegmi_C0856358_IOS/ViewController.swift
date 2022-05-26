dba16ca53958301ec258e3baf43b7845eed8af8c
Sabin Regmi <xawbeenregmi@gmail.com>
2022-05-25 21:19:29 -0400

    Initial Commit

---
+++ FA_SabinRegmi_C0856358_IOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FA_SabinRegmi_C0856358_IOS
+//
+//  Created by Sabin Regmi on 25/05/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
