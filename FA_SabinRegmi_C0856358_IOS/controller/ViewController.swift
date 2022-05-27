47b0a9807fe062a2a87f7c702b4ac9c5981ecead
Sabin Regmi <xawbeenregmi@gmail.com>
2022-05-27 17:08:06 -0400

    Checking For Victory Logic Added
    -- validating victory or draw

--- FA_SabinRegmi_C0856358_IOS/controller/ViewController.swift
+++ FA_SabinRegmi_C0856358_IOS/controller/ViewController.swift
@@ -32,14 +32,73 @@ class ViewController: UIViewController {
     var noughtsScore = 0
     var crossesScore = 0
     
+    // todo: We will use this list to preserve the state of board using core data
+    var boardState = ["", "", "", "", "", "", "", "", ""]
+    
+    
+    //To define the rule for this game / represent the logic of the game
+    let playRules = [
+                // rule for horizontal victory
+                [0,1,2],[3,4,5],[6,7,8],
+                // rule for vertical victory
+                [0,3,6],[1,4,7],[2,5,8],
+                // rule for diagonal victory
+                [0,4,8],[2,4,6]]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        // :todo We need to load the state from core data
+        loadState()
+
     }
     
     // function to handle tap in the board
     @IBAction func boardTapHandler(_ sender: UIButton) {
         addToBoard(sender)
+        
+        // Create an index to get the index of each buttons
+        let i = btnList.firstIndex(of: sender)!
+        
+        // This is because we have already changed the turn of the user when adding to the board
+        if currentTurn == Turn.Cross{
+            boardState[i] = NOUGHT
+        }else{
+            boardState[i] = CROSS
+        }
+        
+        checkForVictory()
+        
+    }
+    
+    func checkForVictory() {
+        for rule in playRules {
+            let firstElementOfRule = boardState[rule[0]]
+            let secondElementOfRule = boardState[rule[1]]
+            let thirdElementOfRule = boardState[rule[2]]
+
+            if firstElementOfRule == secondElementOfRule,
+               secondElementOfRule == thirdElementOfRule,
+               !firstElementOfRule.isEmpty {
+                print("\(firstElementOfRule) is the winner!")
+                // todo: Add Alert
+                
+                // To add score to the players
+                if firstElementOfRule == NOUGHT {
+                    noughtsScore += 1
+                    print(noughtsScore)
+                } else if firstElementOfRule == CROSS {
+                    crossesScore += 1
+                    print(crossesScore)
+                }
+            }
+        }
+        if !boardState.contains(""){
+            print("Its a tie")
+            // todo: Add Alert
+        }
+        
     }
     
     // function to draw X or O when user tap on the board
@@ -60,6 +119,14 @@ class ViewController: UIViewController {
                 turnLabel.text = NOUGHT
             }
             sender.isEnabled = false
+        }
+    }
+    
+    func loadState(){
+        for (index, state) in boardState.enumerated() {
+            if state != ""{
+                addToBoard(btnList[index])
+            }
         }
     }
     
