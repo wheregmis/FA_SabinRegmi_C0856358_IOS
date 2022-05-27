662fb7c8bba6dfa535bc79ee0a804da3c7bbb99c
Sabin Regmi <xawbeenregmi@gmail.com>
2022-05-27 16:02:19 -0400

    Drawing X and O in the board
    -- function to handle tap in the board implemented
    -- function to draw X or O when user tap in the board implemented

--- FA_SabinRegmi_C0856358_IOS/controller/ViewController.swift
+++ FA_SabinRegmi_C0856358_IOS/controller/ViewController.swift
@@ -8,11 +8,61 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    // defining enum for turn
+    enum Turn {
+        case Nought
+        case Cross
+    }
+    
+    @IBOutlet weak var turnLabel: UILabel!
+    
+    @IBOutlet var btnList: [UIButton]!
+    
+    // defining first turn and current turn
+    var firstTurn = Turn.Cross
+    var currentTurn = Turn.Cross
+    
+    // defining string for Nought as O and cross as X
+    var NOUGHT = "O"
+    var CROSS = "X"
+    
+    // defining the score as zero for now
+    // todo: Core data implementation to load score if any
+    var noughtsScore = 0
+    var crossesScore = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    // function to handle tap in the board
+    @IBAction func boardTapHandler(_ sender: UIButton) {
+        addToBoard(sender)
+    }
+    
+    // function to draw X or O when user tap on the board
+    func addToBoard(_ sender: UIButton)
+    {
+        if(sender.title(for: .normal) == nil)
+        {
+            if(currentTurn == Turn.Nought)
+            {
+                sender.setTitle(NOUGHT, for: .normal)
+                currentTurn = Turn.Cross
+                turnLabel.text = CROSS
+            }
+            else if(currentTurn == Turn.Cross)
+            {
+                sender.setTitle(CROSS, for: .normal)
+                currentTurn = Turn.Nought
+                turnLabel.text = NOUGHT
+            }
+            sender.isEnabled = false
+        }
+    }
+    
 
 
 }
